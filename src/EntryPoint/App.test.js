e4911df5a0837b6280e5122e01d9817328173794
Santiago Iraola <iraola.santi@gmail.com>
2021-03-12 11:44:08 -0800

    basic test examples

--- src/EntryPoint/App.test.js
+++ src/EntryPoint/App.test.js
@@ -1,15 +1,31 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 import StringManager from '../utils/StringManager';
-import { BrowserRouter as Router } from "react-router-dom";
+import { useLocation, BrowserRouter as Router } from "react-router-dom";
 
 describe('App', () => {
-  test('it shows a footer with our app and team description', () => {
+
+  beforeEach(() => {
+    // We require to include Router in our test rendering as react-router-dom Link components cannot exist without parent Router
     render(
       <Router>
-        <App />
+        <App/>
       </Router>);
+  });
+  test('it shows a footer with our app and team description', () => {
     const link = screen.getByText(StringManager.get('appOwnershipFooter'));
     expect(link).toBeInTheDocument();
+  });
+
+  test('clicking on Search header redirects to Search Page', () => {
+    fireEvent.click(screen.getByText(StringManager.get('search')));
+    const selectEpitopeButton = screen.getByText(StringManager.get('selectEpitope'));
+    expect(selectEpitopeButton).toBeInTheDocument();
+    expect(window.location.pathname).toEqual('/search');
+  });
+
+  test('clicking on Search header redirects to Search Page', () => {
+    fireEvent.click(screen.getByText(StringManager.get('uploadPatents')));
+    expect(window.location.pathname).toEqual('/upload');
   });
 });
--- src/components/HeaderMenu/HeaderMenu.test.js
+++ src/components/HeaderMenu/HeaderMenu.test.js
@@ -1,5 +1,18 @@
-describe('HeaderMenu', () => {
-    test('test', () => {
+import { render, screen } from '@testing-library/react'
+import HeaderMenu from './HeaderMenu';
+import StringManager from '../../utils/StringManager';
+import { BrowserRouter as Router } from 'react-router-dom';
 
+describe('HeaderMenu', () => {
+    test('upload patents and search links are rendered', () => {
+        render(
+            <Router>
+                <HeaderMenu/>
+            </Router>
+        );
+        const uploadPatentsLink = screen.getByText(StringManager.get('uploadPatents'));
+        const searchLink = screen.getByText(StringManager.get('search'));
+        expect(uploadPatentsLink).toBeInTheDocument();
+        expect(searchLink).toBeInTheDocument();
     });
 });
