c83563169b722c5d4d999d2662adcc7eee0b22f2
Santiago Iraola <iraola.santi@gmail.com>
2021-04-26 21:21:58 -0700

    Actual changes to getProteins

--- amplify/backend/function/getProteins/src/app.js
+++ amplify/backend/function/getProteins/src/app.js
@@ -58,7 +58,7 @@ const convertUrlType = (param, type) => {
  * HTTP Get method for list objects *
  ********************************/
 
-app.get(path + hashKeyPath, function(req, res) {
+app.get(path, function(req, res) {
   var condition = {}
   condition[partitionKeyName] = {
     ComparisonOperator: 'EQ'
@@ -77,10 +77,10 @@ app.get(path + hashKeyPath, function(req, res) {
 
   let queryParams = {
     TableName: tableName,
-    KeyConditions: condition
+    Select: "ALL_ATTRIBUTES"
   }
 
-  dynamodb.query(queryParams, (err, data) => {
+  dynamodb.scan(queryParams, (err, data) => {
     if (err) {
       res.statusCode = 500;
       res.json({error: 'Could not load items: ' + err});
